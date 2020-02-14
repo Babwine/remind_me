f82fb376b92d874bad01d6504189461c2683cc6d
root <root@DESKTOP-2DE21MM.localdomain>
2020-02-14 12:20:57 +0100

    Ajout de la documentation des fichiers du package pfe.remindme.data.entity

--- application/app/src/main/java/pfe/remindme/data/entity/NoteEntity.java
+++ application/app/src/main/java/pfe/remindme/data/entity/NoteEntity.java
@@ -10,36 +10,75 @@ import java.util.List;
 
 import pfe.remindme.data.DataConverter;
 
+/**
+ * La classe représentant l'entité des notes qui sera le modèle des notes stockées en base de donnée
+ */
 @Entity
 public class NoteEntity {
+    /**
+     * L'ID de la note
+     */
     @NonNull
     @PrimaryKey(autoGenerate = true)
     public int id;
+
+    /**
+     * Le contenu de la note
+     */
     public String content;
+
+    /**
+     * La liste des chaînes de caractères correspondant aux noms des tags associés à la note
+     */
     @TypeConverters(DataConverter.class)
     @ColumnInfo(name = "tagList")
     List<String> tagList;
 
+
+    /**
+     * Renvoie l'ID de cette NoteEntity
+     * @return l'ID de cette NoteEntity
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Modifie l'ID de la note en l'initialisant à <code>id</code>
+     * @param id l'ID donné
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Renvoie le contenu de cette NoteEntity
+     * @return le contenu de cette NoteEntit
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * Modifie le contenu de la note en l'initialisant à <code>content</code>
+     * @param content le contenu donné
+     */
     public void setContent(String content) {
         this.content = content;
     }
 
+    /**
+     * Renvoie la liste des noms des tags associés à cette NoteEntity
+     * @return la liste des noms des tags associés à cette NoteEntity
+     */
     public List<String> getTagList() {
         return tagList;
     }
 
+    /**
+     * Modifie la liste des noms des tags associés à la note en l'initialisant à <code>tagList</code>
+     * @param tagList la liste donnée
+     */
     public void setTagList(List<String> tagList) {
         this.tagList = tagList;
     }
--- application/app/src/main/java/pfe/remindme/data/entity/TagEntity.java
+++ application/app/src/main/java/pfe/remindme/data/entity/TagEntity.java
@@ -12,29 +12,50 @@ import pfe.remindme.data.DataConverter;
 
 @Entity
 public class TagEntity {
+    /**
+     * Le nom du tag
+     */
     @NonNull
     @PrimaryKey
     String tagName;
 
+    /**
+     * La liste des ID des notes associées à cette TagEntity
+     */
     @TypeConverters(DataConverter.class)
     @ColumnInfo(name = "linkedNotesIdList")
     List<Integer> linkedNotesIdList;
 
-
+    /**
+     * Renvoie la liste des ID des notes associées à cette TagEntity
+     * @return la liste des ID des notes associées à cette TagEntity
+     */
     @TypeConverters(DataConverter.class)
     public List<Integer> getLinkedNotesIdList() {
         return linkedNotesIdList;
     }
 
+    /**
+     * Modifie la liste des ID des notes associées au tag en l'initialisant à <code>linkedNotesIdList</code>
+     * @param linkedNotesIdList la liste donnée
+     */
     public void setLinkedNotesIdList(List<Integer> linkedNotesIdList) {
         this.linkedNotesIdList = linkedNotesIdList;
     }
 
+    /**
+     * Renvoie le nom de cette TagEntity
+     * @return le nom de cette TagEntity
+     */
     @NonNull
     public String getTagName() {
         return tagName;
     }
 
+    /**
+     * Modifie le nom du tag en l'initialisant à <code>tagName</code>
+     * @param tagName le nom donné
+     */
     public void setTagName(@NonNull String tagName) {
         this.tagName = tagName;
     }

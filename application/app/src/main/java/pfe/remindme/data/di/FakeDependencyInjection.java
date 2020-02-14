d1daa66d99f7e67b325f432ffdfed960c348e3b9
root <root@DESKTOP-2DE21MM.localdomain>
2020-02-14 12:13:56 +0100

    Ajout de la documentation des fichiers du package pfe.remindme.data.di

--- application/app/src/main/java/pfe/remindme/data/di/FakeDependencyInjection.java
+++ application/app/src/main/java/pfe/remindme/data/di/FakeDependencyInjection.java
@@ -15,12 +15,19 @@ import pfe.remindme.data.repository.notedisplay.mapper.TagEntityToTagMapper;
 import pfe.remindme.data.repository.notedisplay.mapper.TagToTagEntityMapper;
 import pfe.remindme.data.repository.notedisplay.remote.NoteDisplayRemoteDataSource;
 
+/**
+ * La classe implémentée en l'absence d'une injection de dépendances plus poussée (cf utilisation de Dagger)
+ */
 public class FakeDependencyInjection {
     private static NoteDisplayRepository noteDisplayRepository;
     private static NoteDatabase noteDatabase;
     private static TagDatabase tagDatabase;
     private static Context applicationContext;
 
+    /**
+     * Initialise si besoin et renvoie le repository de l'application
+     * @return le repository
+     */
     public static NoteDisplayRepository getNoteDisplayRepository() {
         if (noteDisplayRepository == null) {
             noteDisplayRepository = new NoteDisplayDataRepository(
@@ -35,6 +42,10 @@ public class FakeDependencyInjection {
         return noteDisplayRepository;
     }
 
+    /**
+     * Initialise si besoin et renvoie la base de données des notes de l'application
+     * @return la BDD des notes
+     */
     public static NoteDatabase getNoteDatabase() {
         if (noteDatabase == null) {
             noteDatabase = Room.databaseBuilder(applicationContext,
@@ -44,6 +55,10 @@ public class FakeDependencyInjection {
         return noteDatabase;
     }
 
+    /**
+     * Initialise si besoin et renvoie la base de données des tags de l'application
+     * @return la BDD des tags
+     */
     public static TagDatabase getTagDatabase() {
         if (tagDatabase == null) {
             tagDatabase = Room.databaseBuilder(applicationContext,
@@ -53,6 +68,10 @@ public class FakeDependencyInjection {
         return tagDatabase;
     }
 
+    /**
+     * Initialise le contexte de l'application
+     * @param context le contexte d'initialisation
+     */
     public static void setContext(Context context) {
         applicationContext = context;
     }
